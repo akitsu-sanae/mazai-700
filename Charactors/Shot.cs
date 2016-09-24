8135ca888c9e9b20dba87e79d33fca0e63af2d98
akitsu-sanae <akitsu.sanae@gmail.com>
2016-09-24 22:08:36 +0900

    add shot

---
+++ Charactors/Shot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mazai_700.Charactors
+{
+    class Shot : Charactor
+    {
+        public Shot(asd.Vector2DF pos)
+        {
+            Position = pos;
+            AddGraph(new Graph(new asd.Color(255, 255, 255), ":"));
+            base.OnUpdate();
+        }
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+            Position += new asd.Vector2DF(0, -5);
+            if (Position.X < -32 || Consts.Window.Width < Position.X + 32 ||
+                Position.Y < -32 || Consts.Window.Height < Position.Y + 32)
+                Dispose();
+        }
+    }
+}
--- Scene/Game.cs
+++ Scene/Game.cs
@@ -10,9 +10,23 @@ namespace mazai_700.Scene
     {
         public Game()
         {
-            var layer = new asd.Layer2D();
-            layer.AddObject(new Charactors.Player());
-            AddLayer(layer);
+            gameLayer.AddObject(player);
+            AddLayer(gameLayer);
         }
+
+        protected override void OnUpdated()
+        {
+            if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Z) == asd.KeyState.Push)
+            {
+                gameLayer.AddObject(new Charactors.Shot(player.Position + new asd.Vector2DF(24, 0)));
+                gameLayer.AddObject(new Charactors.Shot(player.Position + new asd.Vector2DF(-24, 0)));
+            }
+
+
+        }
+
+        asd.Layer2D gameLayer = new asd.Layer2D();
+        Charactors.Player player = new Charactors.Player();
+
     }
 }
